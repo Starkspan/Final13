fc518a7c5b4458b48be975b388c6c24014296f67
Starkspan <info@kuntsolutions.de>
2025-05-27 13:51:56 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require("express");
+const multer = require("multer");
 const app = express();
 const PORT = process.env.PORT || 10000;
+
+const upload = multer({ dest: "uploads/" });
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Starkspan Backend läuft.");
+});
+
+app.post("/analyze", upload.single("file"), (req, res) => {
+  console.log("Analyse gestartet");
+  res.json({ message: "Analyse erfolgreich empfangen." });
 });
 
 app.listen(PORT, () => {
